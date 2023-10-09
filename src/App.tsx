b9de650cc42b3ba3e950c745a24aad033ef2e6e2
Ali Soliman <alisolimanworks@gmail.com>
2023-10-09 22:11:31 +0300

    Setup the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+// import "./App.scss";
+import Layout from "./Layout/Layout";
+import * as colors from "@mui/material/colors";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: blueGrey[900],
+  //   },
+  //   secondary: {
+  //     main: pink[500],
+  //   },
+  //   background: {
+  //     paper: grey[100],
+  //     default: grey[300],
+  //   },
+  // },
+  palette: {
+    // primary: {
+    //   main: "#0091D5",
+    // },
+    secondary: {
+      main: "#EA6A47",
+      // main: colors.deepOrange[500],
+    },
+    background: {
+      paper: "#FFFFFF",
+      default: "#F1F1F1",
+    },
+    // text: {
+    //   primary: "#444850",
+    // },
+  },
+});
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Layout />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+function MainPage() {
+  return <Box>Hello There</Box>;
+}
+
+export default MainPage;
