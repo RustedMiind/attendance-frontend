dbc1f449dcbf93075a3219517f99f08875b3ca1d
Ali Soliman <alisolimanworks@gmail.com>
2023-10-17 21:39:03 +0300

    Toaster Added

--- src/pages/roles/components/search-create-bar/CreateNewRoleDialog.tsx
+++ src/pages/roles/components/search-create-bar/CreateNewRoleDialog.tsx
@@ -6,11 +6,14 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { Stack } from "@mui/material";
+import { Alert, Snackbar, Stack } from "@mui/material";
 import PermissionInput from "./PermissionInput";
 import axios from "axios";
 import api from "../../../../statics/api";
-import { ApiSuccessfullResponse } from "../../../../types/ApiResponses";
+import {
+  ApiErrorResponse,
+  ApiSuccessfullResponse,
+} from "../../../../types/ApiResponses";
 import { PermissionType } from "../../../../types/Permission";
 import {
   CompressedPermissionType,
@@ -22,24 +25,29 @@ import { RoleType } from "../../../../types/Role";
 export default function CreateNewRoleDialog(props: PropsType) {
   const handleClose = props.close;
   const [name, setName] = React.useState("");
-  const [status, setStatus] = React.useState<"none" | "loading" | "error">(
-    "none"
-  );
+  const [status, setStatus] = React.useState<
+    "none" | "loading" | "error" | "success"
+  >("none");
+
+  const [error, setError] = React.useState<null | string>(null);
   const isLoading = status === "loading";
   const isError = status === "error";
   function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
     setName(e.target.value);
   }
-  function ResetDialog() {}
+  function ResetDialog() {
+    setError(null);
+    setPermissions(null);
+    setName("");
+    setStatus("none");
+  }
   let [permissions, setPermissions] = React.useState<
     null | CompressedPermissionTypeWithSelect[]
   >(null);
 
   React.useEffect(() => {
     if (props.open) {
-      setPermissions(null);
-      setName("");
-      setStatus("none");
+      ResetDialog();
       setTimeout(() => {
         axios
           .get<
@@ -51,12 +59,15 @@ export default function CreateNewRoleDialog(props: PropsType) {
           .then((res) => {
             setPermissions(res.data.data.compressedPermissions);
             console.log(res.data.data);
+          })
+          .catch((err: ApiErrorResponse<any>) => {
+            setError(err.response.data.message);
           });
       }, 800);
     }
   }, [props.open]);
 
-  function setSelectGenerator(permissionIndex: number) {
+  function setSelectGenerate(permissionIndex: number) {
     return function (actionId: number) {
       if (permissions) {
         permissions[permissionIndex].select = actionId;
@@ -67,6 +78,88 @@ export default function CreateNewRoleDialog(props: PropsType) {
     };
   }
 
+  return (
+    <>
+      <Dialog
+        component={"form"}
+        maxWidth="md"
+        fullWidth
+        open={props.open}
+        onClose={handleClose}
+        onSubmit={submitHandler}
+      >
+        <DialogTitle>Create New Role</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            You Can Only create role if you have role write permission and you
+            can only give permissions you have or permissions below
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Role Name"
+            type="text"
+            variant="outlined"
+            fullWidth
+            value={name}
+            disabled={isLoading}
+            onChange={handleNameChange}
+            error={isError}
+            helperText={error}
+          />
+          <Stack my={2}>
+            {permissions?.map((permission, i) => (
+              <PermissionInput
+                key={permission.name}
+                permission={permission}
+                setSelect={setSelectGenerate(i)}
+                select={permission.select}
+                isLoading={isLoading}
+                isError={isError}
+              />
+            ))}
+          </Stack>
+        </DialogContent>
+        <DialogActions sx={{ px: 3, pb: 2 }}>
+          <Button
+            color={isError ? "error" : undefined}
+            disabled={isLoading}
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <LoadingButton
+            variant="contained"
+            loading={isLoading}
+            // onClick={extractRoleData}
+            type="submit"
+            color={isError ? "error" : undefined}
+          >
+            Create Role
+          </LoadingButton>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={status === "success" || isError}
+        autoHideDuration={6000}
+        onClose={() => {
+          setStatus("none");
+        }}
+      >
+        <Alert
+          onClose={() => {
+            setStatus("none");
+          }}
+          severity={isError ? "error" : "success"}
+          sx={{ width: "100%" }}
+        >
+          {isError ? error : "Role has been created successfully"}
+        </Alert>
+      </Snackbar>
+    </>
+  );
+  // Submit Handler
   function submitHandler(e: React.FormEvent<HTMLDivElement>) {
     e.preventDefault();
     setStatus("loading");
@@ -88,7 +181,7 @@ export default function CreateNewRoleDialog(props: PropsType) {
             })
             .then((res) => {
               console.log("update Roles", res);
-              setStatus("none");
+              setStatus("success");
               props.setRoles(res.data.data);
               props.close();
             })
@@ -104,69 +197,7 @@ export default function CreateNewRoleDialog(props: PropsType) {
       }
     }, 1500);
   }
-
-  return (
-    <Dialog
-      component={"form"}
-      maxWidth="md"
-      fullWidth
-      open={props.open}
-      onClose={handleClose}
-      onSubmit={submitHandler}
-    >
-      <DialogTitle>Create New Role</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          You Can Only create role if you have role write permission and you can
-          only give permissions you have or permissions below
-        </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Role Name"
-          type="text"
-          variant="outlined"
-          fullWidth
-          value={name}
-          disabled={isLoading}
-          onChange={handleNameChange}
-          error={isError}
-          helperText={isError ? "An error occured" : undefined}
-        />
-        <Stack my={2}>
-          {permissions?.map((permission, i) => (
-            <PermissionInput
-              key={permission.name}
-              permission={permission}
-              setSelect={setSelectGenerator(i)}
-              select={permission.select}
-              isLoading={isLoading}
-              isError={isError}
-            />
-          ))}
-        </Stack>
-      </DialogContent>
-      <DialogActions sx={{ px: 3, pb: 2 }}>
-        <Button
-          color={isError ? "error" : undefined}
-          disabled={isLoading}
-          onClick={handleClose}
-        >
-          Cancel
-        </Button>
-        <LoadingButton
-          variant="contained"
-          loading={isLoading}
-          // onClick={extractRoleData}
-          type="submit"
-          color={isError ? "error" : undefined}
-        >
-          Create Role
-        </LoadingButton>
-      </DialogActions>
-    </Dialog>
-  );
+  // End Submit Handler
 }
 
 type PropsType = {
